bb940ff65a4d944308bea835f22b8091c4abe431
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-04 19:00:09 +0530

    new solution

--- ProblemSolving/number_line_jumps.go
+++ ProblemSolving/number_line_jumps.go
@@ -31,7 +31,6 @@ func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
     secondAnimal := x2 + v2
     initialResult := firstAnimal - secondAnimal
     result := firstAnimal - secondAnimal
-    
     for result != 0 {
         firstAnimal = firstAnimal + v1
         secondAnimal = secondAnimal + v2
